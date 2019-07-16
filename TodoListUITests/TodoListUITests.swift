0f28d37bddd3a4d600fa336c7581518a46b619b2
Usama Nasar <usamanasar38@gmail.com>
2019-07-16 14:21:41 +0500

    Some UI Tests

--- TodoListUITests/TodoListUITests.swift
+++ TodoListUITests/TodoListUITests.swift
@@ -9,26 +9,59 @@
 import XCTest
 
 class TodoListUITests: XCTestCase {
-
+    var app: XCUIApplication!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        super.setUp()
+        app = XCUIApplication()
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        //        XCUIApplication().launch()
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
+        super.tearDown()
     }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testSignUpButtonDisplays() {
+        app.launch()
+        XCTAssertTrue(app.buttons["Sign up"].exists)
     }
-
+    
+    func testSignUpButtonShowsSignUpUi() {
+        app.launch()
+        app.buttons["Sign up"].tap()
+        XCTAssertFalse(app.buttons["Sign in"].exists)
+    }
+    
+    func testsignup() {
+        app.launch()
+        app.buttons["Sign up"].tap()
+        let alert = app.alerts["Register"]
+        let user = "user@user.com"
+        alert.typeText("\(user)\n")
+        alert.typeText("123456\n")
+    }
+    
+    func testlogin() {
+        app.launch()
+        let email = app.textFields["Email"]
+        //        let password = app.textFields["Password"]
+        let user = "user@user.com"
+        if email.exists{
+            email.tap()
+            email.typeText("\(user)\n")
+            app.typeText("123456")
+            app.buttons["Login"].tap()
+        }
+        app.buttons["User"].tap()
+    }
+    
 }
